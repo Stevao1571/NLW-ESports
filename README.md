4d97b16d6c435e2fa81706790a095f76df7c5e65
Stevão Gacia de Souza <stevaoo@hotmail.com>
2022-09-27 19:00:10 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,52 @@
+<p align="center">
+  <img alt="" src=".github/capa.png" width="100%">
+</p>
+
+Este projeto foi desenvolvido em um treinamento online disponibilizado pela Roketseat.
+
+É uma boa fonte de aprendizado, bem como de aprimoramento.
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+# NLW - eSports
+
+## 📋 Conceito
+![nlw esports banner](././.github/nlw-esports-banner.png)
+
+## 🚀 Tecnologias
+
+### Front-end
+- HTML
+- CSS
+- JavaScript
+- TypeScript
+- ReactJS
+- TailwindCSS
+- Radix UI
+
+### Back-end
+- NodeJS
+- Express
+- Prisma
+- SQLite
+
+### Mobile
+- React Native
+- Expo
+
+
+## 🎨 Layout:
+
+Figma: https://www.figma.com/file/fMH9LY1h1ngivGZBuq7zm5/NLW-eSports-(Community)?node-id=6%3A131
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+By Stevão Souza
---
+++ mobile/src/screnns/index.tsx
@@ -0,0 +1,56 @@
+import{useEffect, useState} from 'react';
+import { Image, FlatList } from "react-native";
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard, GameCardProps } from "../components/GameCard";
+import { Heading } from "../components/Heading";
+import { styles } from './styles';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export function Home(){
+
+    const[games, setGames] = useState<GameCardProps[]>([]);
+
+    useEffect( () =>{
+        fetch('http://192.168.0.111:3333/games')
+        .then(response => response.json())
+        .then(data => setGames(data))
+    }, []);
+    
+
+    return (
+
+        <SafeAreaView style={styles.container}>
+
+            <Image
+            source ={logoImg}
+            style={styles.logo}
+            />
+            <Heading
+            
+            title="Encontre seu duo!"
+            subtitle="Selecione o game que deseja jogar..."
+            />
+
+            <FlatList
+                data={games}
+                keyExtractor={item=>item.id}
+                
+                renderItem={({item})=>(
+                    <GameCard            
+                    data={item}            
+                    />
+
+                )}
+                showsHorizontalScrollIndicator={false}
+                horizontal  
+                
+                contentContainerStyle={styles.contentList}
+
+                
+             />
+        
+
+        </SafeAreaView>
+
+    );
+}
